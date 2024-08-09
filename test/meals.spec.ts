81f922271e106bf501b835446fcd3577fdcbbf96
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-08-08 21:48:15 -0300

    feat: rota com todas as metricas do usuario

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -179,4 +179,76 @@ describe('Meals routes', () => {
       .set('Cookie', userResponse.get('Set-Cookie') ?? [])
       .expect(204)
   })
+
+  it('should be able to get metrics from a user', async () => {
+    const userResponse = await request(app.server)
+      .post('/users')
+      .send({ name: 'John Doe', email: 'johndoe@gmail.com' })
+      .expect(201)
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', userResponse.get('Set-Cookie') ?? [])
+      .send({
+        name: 'Breakfast',
+        description: "It's a breakfast",
+        isOnDiet: true,
+        date: new Date('2021-01-01T08:00:00'),
+      })
+      .expect(201)
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', userResponse.get('Set-Cookie') ?? [])
+      .send({
+        name: 'Lunch',
+        description: "It's a lunch",
+        isOnDiet: false,
+        date: new Date('2021-01-01T12:00:00'),
+      })
+      .expect(201)
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', userResponse.get('Set-Cookie') ?? [])
+      .send({
+        name: 'Snack',
+        description: "It's a snack",
+        isOnDiet: true,
+        date: new Date('2021-01-01T15:00:00'),
+      })
+      .expect(201)
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', userResponse.get('Set-Cookie') ?? [])
+      .send({
+        name: 'Dinner',
+        description: "It's a dinner",
+        isOnDiet: true,
+        date: new Date('2021-01-01T20:00:00'),
+      })
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', userResponse.get('Set-Cookie') ?? [])
+      .send({
+        name: 'Breakfast',
+        description: "It's a breakfast",
+        isOnDiet: true,
+        date: new Date('2021-01-02T08:00:00'),
+      })
+
+    const metricsResponse = await request(app.server)
+      .get('/meals/metrics')
+      .set('Cookie', userResponse.get('Set-Cookie') ?? [])
+      .expect(200)
+
+    expect(metricsResponse.body).toEqual({
+      totalMeals: 5,
+      totalMealsOnDiet: 4,
+      totalMealsOffDiet: 1,
+      bestOnDietSequence: 3,
+    })
+  })
 })
